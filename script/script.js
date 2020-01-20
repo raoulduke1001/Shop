c9042ba4c373f013ba8b0c51323c6f7bdea9e1a8
raoulduke1001 <1raoulduke1@gmail.com>
2020-01-20 12:34:08 +0300

    0.1

---
+++ script/script.js
@@ -0,0 +1,69 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const search = document.querySelector('.search');
+    const cartBtn = document.getElementById('cart');
+    const wishList = document.getElementById('wishlist');
+    const goodsWrapper = document.querySelector('.goods-wrapper');
+    const cart = document.querySelector('.cart');
+
+    const createCardGoods = (id, title, price, img) => {
+        const card = document.createElement('div');
+        card.className = 'card-wrapper col-12 col-md-6 col-lg-4 col-xl-3 pb-3';
+        card.innerHTML = `<div class="card">
+            <div class="card-img-wrapper">
+            <img class="card-img-top" src="${img}" alt="">
+            <button class="card-add-wishlist"
+            data-goods-id="${id}"></button>
+            </div>
+            <div class="card-body justify-content-between">
+            <a href="#" class="card-title">"${title}"</a>
+            <div class="card-price">${price}</div>
+            <div>
+            <button class="card-add-cart"
+            data-goods-id="${id}">Добавить в корзину</button>
+            </div>
+            </div>
+            </div>`;
+        return card;
+    };
+
+    const closeCart = (e) => {
+        const target = e.target;
+        if (target === cart ||
+            target.classList.contains('cart-close') ||
+            e.key === 'Escape') {
+            cart.style.display = '';
+            document.removeEventListener('keydown', closeCart);
+        }
+    };
+
+    const openCart = (e) => {
+        e.preventDefault();
+        cart.style.display = 'flex';
+        document.addEventListener('keydown', closeCart);
+    };
+    cartBtn.addEventListener('click', openCart);
+    cart.addEventListener('click', closeCart);
+
+    const renderCard = (items) => {
+        goodsWrapper.textContent = "";
+        items.forEach(({
+            id,
+            title,
+            price,
+            imgMin
+        }) => {
+            goodsWrapper.appendChild(createCardGoods(id, title, price, imgMin));
+        })
+    };
+
+
+    const getGoods = (handler) => {
+        fetch('db/db.json')
+            .then(response => response.json())
+            .then(handler)
+            console.log(handler);
+    };
+
+    getGoods(renderCard);
+
+});
